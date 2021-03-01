48a9fe6dbd414beb3a62540ca6bdd0eb4f89a709
Omar Lamprea <david38595@gmail.com>
2021-03-01 11:39:38 -0500

    add short cuts and countdown inputs to html

--- js/countdown.js
+++ js/countdown.js
@@ -1,49 +1,54 @@
 export default function countdownClock(countdown, date){
   const d = document;
   const $clock = d.querySelector(countdown)
-
-  const dateTarget = new Date(date)
-
+  const $formDate = d.getElementById(date)
+  
   const $days = d.querySelector('#days')
   const $hours = d.querySelector('#hours')
   const $minutes = d.querySelector('#minutes')
   const $seconds = d.querySelector('#seconds')
-
+  
   // console.log(days, hours, minutes, seconds)
-
+  
   const sec = 1000
   const min = sec * 60
   const hr = min * 60
   const day = hr * 24
-
-  let stopCountdown = 
-  setInterval(() => {
-    remaining()
-  }, 1000);
+  
+  
   
   function remaining(){
+    const dateTarget = new Date($formDate.date.value)
     const dateA = new Date()
     const duration = dateTarget - dateA
-
+    
     const remainingDays = Math.floor(duration / day)
     const remainingHours = Math.floor((duration % day) / hr)
     const remainingMinutes = Math.floor((duration % hr) / min)
     const remainingSeconds = Math.floor((duration % min) / sec)
-
+    
 
     $days.innerHTML = `${remainingDays} días `
     $hours.innerHTML = `${remainingHours} Horas `
     $minutes.innerHTML = `${remainingMinutes} minutos `
     $seconds.innerHTML = `${remainingSeconds} segundos `
     
-    // console.log(duration)
+    let stopCountdown = 
+      setInterval(() => {
+        remaining()
+      }, 1000);
+
     if(remainingSeconds < 0){
       // alert('feliz cumpleaños')
       clearInterval(stopCountdown)
       $clock.innerHTML = `<h3>Feliz Cumpleaños!🤓</h3>`
     }
   }
-  
-  remaining()
-  
+
+  d.addEventListener('click', (e)=>{
+    if(e.target === $formDate.dateBtn){
+      remaining()
+    }
+  })
+
 }
--- js/ejercicios.js
+++ js/ejercicios.js
@@ -19,7 +19,7 @@ d.addEventListener('DOMContentLoaded',(e)=>{
   //Exercise 2: ball- events
   moveBall()
   //Exercise 3: countdown
-  countdownClock('#countdown', '2021, 10, 06')
+  countdownClock('#countdown', 'form-date')
   //Excercise: scroll
   scrollTop()
   //Excersice 4: responsive JS
@@ -27,13 +27,13 @@ d.addEventListener('DOMContentLoaded',(e)=>{
     'video',
     '(min-width: 600px)',
     `<a target="_blank" href="https://www.youtube.com/watch?v=Sp4ELDWoIsk&feature=emb_title" blank_>Ver Video</a>`,
-    // `<iframe class="youtube-video" width="560" height="315" src="https://www.youtube.com/embed/Sp4ELDWoIsk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+    `<iframe class="youtube-video" width="560" height="315" src="https://www.youtube.com/embed/Sp4ELDWoIsk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
   )
   jsResponsive(
     'gmaps',
     '(min-width: 600px)',
     `<a target="_blank" href="https://www.google.com/maps/" blank_>Ver Mapa</a>`,
-    // `<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d77895.71043505364!2d-75.68411095268995!3d4.5319371564338375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sco!4v1614351040174!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`
+    `<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d77895.71043505364!2d-75.68411095268995!3d4.5319371564338375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sco!4v1614351040174!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`
   )
   //Excersice 5: 
   responsiveTester('responsiveTester')
