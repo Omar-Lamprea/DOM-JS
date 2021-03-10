735b6956e0b8aa98f6d6974d9cd7732c3a634b73
Omar Lamprea <david38595@gmail.com>
2021-03-10 16:24:00 -0500

    Search filter OK, lottery V1

---
+++ js/sorteo.js
@@ -0,0 +1,40 @@
+const d = document
+
+export default function lottery(arrLg, winnerBtn){
+  const players = d.getElementById(arrLg)
+  const playBtn = d.getElementById(winnerBtn)
+  // console.log(players, playBtn)
+
+  const lgs = [
+    'JavaScript',
+    'PHP',
+    'JAVA',
+    'C',
+    'Python',
+    'Ruby',
+    'Go',
+    'Visual Basic',
+    'Rust',
+    'Perl'
+  ]
+
+  const ul = d.createElement('ul')
+  
+  for(let i = 0; i < lgs.length; i++){
+    // console.log(i)
+  }
+  
+  playBtn.addEventListener('click', (e)=>{
+    const winner = (Math.floor(Math.random() * (lgs.length - 0) + 0))
+    console.log(`posición: ${winner}, nombre: ${lgs[winner]}`)
+    alert(`el ganador es: ${lgs[winner]}`)
+  })
+
+  lgs.forEach(el =>{
+    // console.log(el)
+    players.appendChild(ul).innerHTML += `<li>${el}</li>`
+  })
+
+
+
+}
